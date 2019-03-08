4e3841ea7aa29ea6d14bdd1bb56c41a24190dea0
obitech <obitech@users.noreply.github.com>
2019-03-08 20:35:01 +0100

    Added pbpaste to alias.zsh

--- alias.zsh
+++ alias.zsh
@@ -9,3 +9,9 @@ alias ta="tmux attach -t"
 alias tl="tmux list-session"
 alias ts="tmux new-session -s"
 alias tks="tmux kill-session -t"
+
+# Clipboard
+if [ ! $(uname -s) = "Darwin" ]; then
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+fi
